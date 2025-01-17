461b2a73ed952cacc1a2e039feb672c59e23c4ba
Tama <mytamhuynhhmt@gmail.com>
2025-01-17 11:13:47 +0700

    Nội dung 4

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,16 +14,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView txt2;
-    EditText edittext1;
-    TextView txt1;
     Button btn1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +42,21 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        TextView txtChan = findViewById(R.id.textChan);
+        TextView txtLe = findViewById(R.id.textLe);
+        Button btnSoNgauNhien = findViewById(R.id.buttonSo);
+
+        btnSoNgauNhien.setOnClickListener(v -> {
+            ArrayList<Integer> listRandom = generateRandomNumbers(30);
+
+            ArrayList<Integer> listChan = new ArrayList<>();
+            ArrayList<Integer> listLe = new ArrayList<>();
+
+            classifyNumbers(listRandom, listChan, listLe);
+            displayNumbers(listChan, txtChan, "even");
+            displayNumbers(listLe, txtLe, "odd");
         });
 
 
@@ -62,6 +78,37 @@ public class MainActivity extends AppCompatActivity {
         List<String> words = Arrays.asList(input.split(" "));
         Collections.reverse(words);
         return String.join(" ", words).toUpperCase();
+    }
+
+    private ArrayList<Integer> generateRandomNumbers(int count) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            list.add((int) (Math.random() * 100));
+        }
+        return list;
+    }
+
+    private void classifyNumbers(ArrayList<Integer> listRandom, ArrayList<Integer> listChan, ArrayList<Integer> listLe) {
+        for (int number : listRandom) {
+            if (number % 2 == 0) {
+                listChan.add(number);
+            } else {
+                listLe.add(number);
+            }
+        }
+    }
+
+    private void displayNumbers(ArrayList<Integer> list, TextView textView, String type) {
+        Log.d("MainActivity", "Displaying numbers: " + list.toString());
+        if (Objects.equals(type, "even")) {
+            textView.setText("Số chẵn: ");
+        } else {
+            textView.setText("Số lẻ: ");
+
+        }
+        for (int number : list) {
+            textView.append(number + ", ");
+        }
     }
 
 }
